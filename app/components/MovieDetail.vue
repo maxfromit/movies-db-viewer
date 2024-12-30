4c8fdcb1d757cbd53fc4ea28d652622b0e59d3de
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 01:40:38 +0300

    app: add MovieDetail component and movie page to display movie details

---
+++ app/components/MovieDetail.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Movie } from "@/pages/index.vue"
+
+const props = defineProps<{
+  movieId: number
+}>()
+
+// const emit = defineEmits<{
+//   (e: "selected-movie", id: number): void
+// }>()
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-4">{{ props.movieId }}</div>
+</template>
---
+++ app/pages/movie/[[Id]].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const movieId = computed(() => route?.params?.Id)
+</script>
+
+<template>
+  <MovieDetail
+    v-if="movieId && typeof movieId === 'string'"
+    :movie-id="parseInt(movieId, 10)"
+  />
+</template>
