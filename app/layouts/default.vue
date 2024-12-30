a8cec80b1e755e49cbc5dd3ab4f7140f626b4cfe
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 01:40:23 +0300

    app: add default layout with HeaderSection and FooterSection components

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col h-dvh">
+    <HeaderSection />
+    <div class="grow">
+      <slot />
+    </div>
+    <FooterSection />
+  </div>
+</template>
