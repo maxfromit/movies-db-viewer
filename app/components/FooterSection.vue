790c70560aa08bd9ee64873f22902cd00b57ee8b
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 01:40:04 +0300

    app: add FooterSection and HeaderSection components with initial layout

---
+++ app/components/FooterSection.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-row justify-between items-center">
+    <div>Test Task, 2022</div>
+    <div>
+      <div>518-363-2171</div>
+      <div>91 Park St. Fairhope, AL 36532</div>
+    </div>
+  </div>
+</template>
