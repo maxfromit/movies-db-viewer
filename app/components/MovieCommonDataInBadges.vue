91c99e48844fe93f5b67028edaa3c3150495af2b
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 07:47:19 +0300

    app/components/MovieCommonDataInBadges - added MovieCommonDataInBadges component with props for rating, genre, and duration

---
+++ app/components/MovieCommonDataInBadges.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import type { Movie } from "@/pages/index.vue"
+
+const props = defineProps<{
+  rating: number
+  genre: string
+  duration: string
+}>()
+
+const getColorByNumber = (number: number) => {
+  if (number >= 7) {
+    return "#039C55"
+  }
+  if (number >= 4) {
+    return "purple"
+  } else {
+    return "#E81A0C"
+  }
+}
+
+const capitalizeFirstLetter = (string: string) => {
+  if (!string) {
+    return ""
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+</script>
+
+<template>
+  <div class="flex gap-1.5 justify-start">
+    <UBadge
+      :label="props?.rating"
+      :style="{ 'background-color': getColorByNumber(props?.rating) }"
+      :ui="{ slots: { base: 'text-white !important' } }"
+      icon="i-lucide-star"
+      size="sm"
+      class="rounded-full"
+    />
+    <UBadge
+      :label="capitalizeFirstLetter(props?.genre)"
+      style="background-color: #2c81fb"
+      icon="i-lucide-film"
+      size="xs"
+      class="rounded-full"
+    />
+    <UBadge
+      :label="props?.duration"
+      style="background-color: #702bfe"
+      icon="i-lucide-clock"
+      size="xs"
+      class="rounded-full"
+    />
+  </div>
+</template>
