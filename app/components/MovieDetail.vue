1512f81f82044198704b9fb5708c6f4e79f2ce1f
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 07:47:33 +0300

    app/components/MovieDetail - updated movie detail component to use movie object and display additional information

--- app/components/MovieDetail.vue
+++ app/components/MovieDetail.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import type { Movie } from "@/pages/index.vue"
+import type { Movie } from "@/pages/movie/[[id]].vue"
 
 const props = defineProps<{
-  movieId: number
+  movie: Movie
 }>()
 
 // const emit = defineEmits<{
@@ -11,5 +11,57 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-4">{{ props.movieId }}</div>
+  <div class="grid sm:grid-cols-2 md:grid-cols-8 lg:grid-cols-12 gap-14">
+    <div class="flex flex-col gap-4 md:col-span-3 lg:col-span-2">
+      <div class="text-3xl">{{ props?.movie?.name }}</div>
+      <NuxtImg
+        src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/three_thousand_years_of_longing_240x360_crop_center.progressive.jpg?v=1656957331"
+        :alt="props?.movie?.name"
+        loading="lazy"
+        @error="console.error"
+        class="w-full"
+      />
+      <MovieCommonDataInBadges
+        :rating="props?.movie?.rating"
+        :genre="props?.movie?.genre"
+        :duration="props?.movie?.duration"
+      />
+    </div>
+
+    <div class="flex flex-col gap-4 md:col-span-4 lg:col-span-3">
+      <div class="text-3xl">Description</div>
+      <div class="text-xs">{{ props?.movie?.description }}</div>
+      <div class="text-3xl">Trivia</div>
+      <div class="ml-5">
+        <ul
+          v-for="(trivia, index) in props?.movie?.trivia"
+          :key="index"
+          class="list-disc flex flex-col gap-1 text-xs"
+        >
+          <li>
+            {{ trivia }}
+          </li>
+        </ul>
+      </div>
+      <div class="text-3xl">Actors</div>
+      <div class="ml-5">
+        <ul
+          v-for="(actor, key) in props?.movie?.actors"
+          :key="key"
+          class="list-disc flex flex-col gap-1 text-xs"
+        >
+          <li>
+            <ULink
+              raw
+              :href="`https://www.imdb.com/name/${actor?.imdb_id}`"
+              target="_blank"
+              style="text-decoration: underline"
+            >
+              {{ actor?.name }}
+            </ULink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
