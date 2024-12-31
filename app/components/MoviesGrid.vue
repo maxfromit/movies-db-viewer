13b1f8a511b65dba8384ac72e50d480958418e03
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 12:30:20 +0300

    app/components/MoviesGrid updated layout for improved responsiveness and enhanced movie card presentation

--- app/components/MoviesGrid.vue
+++ app/components/MoviesGrid.vue
@@ -11,37 +11,43 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="grid sm:grid-cols-2 md:grid-cols-4 grid-cols-6 gap-4">
+  <div
+    class="grid sm:grid-cols-[1fr_1fr] md:grid-cols-[1fr_1fr_1fr] lg:grid-cols-[1fr_1fr_1fr_1fr] gap-4"
+  >
     <ULink
       v-for="movie in props?.movies"
       :key="movie?.id"
       raw
       :to="`/movie/${movie?.id}`"
+      class="h-full"
     >
-      <UCard
-        class="flex flex-col justify-center hover:shadow-[6px_6px_0_0_rgb(112,43,254)] aspect-[324/306]"
+      <div
+        class="bg-blue-950 rounded-lg flex flex-col content-start hover:shadow-[6px_6px_0_0_rgb(112,43,254)] p-2 h-full"
       >
-        <div class="aspect-[300/172]">
-          <NuxtImg
-            :src="movie?.poster"
-            :alt="movie?.name"
-            width="300"
-            height="172"
-            loading="lazy"
-            class="w-full mb-2"
-            @error="console.error"
-          />
+        <NuxtImg
+          :src="movie?.poster"
+          :alt="movie?.name"
+          width="300"
+          height="172"
+          loading="lazy"
+          class="w-full mb-2 rounded-lg"
+          @error="console.error"
+        />
+        <div class="grow mb-2">
+          <div class="line-clamp-1 custom-h2">
+            {{ movie?.name }}
+          </div>
+          <div class="line-clamp-2 custom-body">
+            {{ movie?.description }}
+          </div>
         </div>
-        <div>
-          <div class="text-xl line-clamp-1">{{ movie?.name }}</div>
-          <div class="text-xs line-clamp-2 mb-2">{{ movie?.description }}</div>
-        </div>
+
         <MovieCommonDataInBadges
           :rating="movie?.rating"
           :genre="movie?.genre"
           :duration="movie?.duration"
         />
-      </UCard>
+      </div>
     </ULink>
   </div>
 </template>
