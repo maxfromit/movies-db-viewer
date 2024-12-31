eabe3b1fc330097c4f8322e34299fde795ec9507
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 07:47:39 +0300

    app/components/MoviesGrid updated layout to improve responsiveness and replaced UCard with ULink for better navigation

--- app/components/MoviesGrid.vue
+++ app/components/MoviesGrid.vue
@@ -5,32 +5,43 @@ const props = defineProps<{
   movies: Movie[]
 }>()
 
-const emit = defineEmits<{
-  (e: "selected-movie", id: number): void
-}>()
+// const emit = defineEmits<{
+//   (e: "selected-movie", id: number): void
+// }>()
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-4">
-    <UCard
-      v-for="movie in props.movies"
-      :key="movie.id"
-      @click="emit('selected-movie', movie.id)"
+  <div class="grid sm:grid-cols-2 md:grid-cols-4 grid-cols-6 gap-4">
+    <ULink
+      v-for="movie in props?.movies"
+      :key="movie?.id"
+      raw
+      :to="`/movie/${movie?.id}`"
     >
-      <NuxtImg
-        :src="movie.poster"
-        :alt="movie.name"
-        :placeholder="[50, 25]"
-        width="300"
-        height="172"
-      />
-      <div>{{ movie.name }}</div>
-      <div>{{ movie.description }}</div>
-      <div>
-        <UBadge :label="movie.rating" />
-        <UBadge :label="movie.genre" />
-        <UBadge :label="movie.duration" />
-      </div>
-    </UCard>
+      <UCard
+        class="flex flex-col justify-center hover:shadow-[6px_6px_0_0_rgb(112,43,254)] aspect-[324/306]"
+      >
+        <div class="aspect-[300/172]">
+          <NuxtImg
+            :src="movie?.poster"
+            :alt="movie?.name"
+            width="300"
+            height="172"
+            loading="lazy"
+            class="w-full mb-2"
+            @error="console.error"
+          />
+        </div>
+        <div>
+          <div class="text-xl line-clamp-1">{{ movie?.name }}</div>
+          <div class="text-xs line-clamp-2 mb-2">{{ movie?.description }}</div>
+        </div>
+        <MovieCommonDataInBadges
+          :rating="movie?.rating"
+          :genre="movie?.genre"
+          :duration="movie?.duration"
+        />
+      </UCard>
+    </ULink>
   </div>
 </template>
