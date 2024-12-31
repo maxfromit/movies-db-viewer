1a798da849260d9a4759552ebd56c6538536bee3
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 07:46:57 +0300

    app/components/FooterSection - updated layout and added contact link in footer

--- app/components/FooterSection.vue
+++ app/components/FooterSection.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-row justify-between items-center">
-    <div>Test Task, 2022</div>
-    <div>
-      <div>518-363-2171</div>
-      <div>91 Park St. Fairhope, AL 36532</div>
+  <div class="bg-blue-950">
+    <div class="flex flex-row justify-between items-center main-padding">
+      <div class="text-lg">Test Task, 2022</div>
+      <div>
+        <div class="text-sm">
+          <ULink href="tel:518-363-2171">518-363-2171</ULink>
+        </div>
+        <div class="text-sm">91 Park St. Fairhope, AL 36532</div>
+      </div>
     </div>
   </div>
 </template>
