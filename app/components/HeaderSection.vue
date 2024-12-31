1007a4f0a43b48c2ccb0da36e5777a8b12ce7bce
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 07:47:07 +0300

    components/HeaderSection - updated header layout and added navigation link

--- app/components/HeaderSection.vue
+++ app/components/HeaderSection.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex">Test Task</div>
+  <div class="flex text-3xl bg-blue-950 font-medium">
+    <ULink raw to="/" class="mx-7 my-5">Test Task</ULink>
+  </div>
 </template>
