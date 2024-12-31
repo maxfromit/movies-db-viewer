a03c3d8dfcb48acae9ea654f05a14f93cf791ec5
maximfromit <bigmaxcapacity@gmail.com>
2024-12-31 07:45:39 +0300

    app: index update to fetch movies using axios and handle errors

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,7 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
 <script setup lang="ts">
+import axios from "axios"
+
 export type Movie = {
   id: number
   name: string
@@ -9,23 +12,52 @@ export type Movie = {
   genre: string
 }
 
-const router = useRouter()
+// const router = useRouter()
 
-const {
-  data: movies,
-  status,
-  error,
-  refresh,
-  clear,
-} = await useAsyncData("movies", () =>
-  $fetch("https://my-json-server.typicode.com/maxfromit/movies-db-viewer/items")
-)
+// const {
+//   data: movies,
+//   status,
+//   error,
+//   refresh,
+//   clear,
+// } = await  useAsyncData("movies", ()=>$fetch(
+//       "https://my-json-server.typicode.com/maxfromit/movies-db-viewer/items"
+//     )
+// )
 
-const goToDetail = (id: number) => {
-  router.push(`/movie/${id}`)
-}
+// const {
+//   data: movies,
+//   status,
+//   error,
+//   refresh,
+//   clear,
+// } = await useAsyncData("movies", async () => {
+//   try {
+//     return await $fetch(
+//       "https://my-json-server.typicode.com/maxfromit/movies-db-viewer/items"
+//     )
+//   } catch (err) {
+//     console.error("Failed to fetch movies:", err)
+//     return null
+//   }
+// })
+const movies = ref<Movie[]>([])
+
+await axios
+  .get("https://my-json-server.typicode.com/maxfromit/movies-db-viewer/items")
+  .then(function (response) {
+    movies.value = response.data
+  })
+  .catch(function (err) {
+    console.error("Failed to fetch movies:", err)
+  })
+
+// const goToDetail = (id: number) => {
+//   router.push(`/movie/${id}`)
+// }
 </script>
 
 <template>
-  <MoviesGrid v-if="movies" :movies="movies" @selected-movie="goToDetail" />
+  <MoviesGrid v-if="movies?.length > 0" :movies="movies" />
+  <div v-else>...</div>
 </template>
